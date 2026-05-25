ab8a52ba78f807fa1ea924156d0c5ecfbbf3fb5f
peepers :3 <106013431@student.swin.edu.au>
2026-05-25 20:37:57 +1000

    added player movement
    added a player character with movement. still need to setup sprite animations. used sample scene for assembly, please make new scenes for actual game content.

---
+++ 2D Game/Assets/Scripts/Player_Movement.cs
@@ -0,0 +1,88 @@
+
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player_Movement : MonoBehaviour
+{
+    private float horizontal;
+    private Vector2 moveInput;
+    private float speed = 8f;
+    private float jumpingPower = 16f;
+    private bool isFacingRight = true;
+
+    private InputSystem_Actions inputSystemActions;
+    private InputAction moveAction;
+    private InputAction jumpAction;
+
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+
+
+    private void Awake()
+    {
+        inputSystemActions = new InputSystem_Actions();
+        moveAction = inputSystemActions.Player.Move;
+        jumpAction = inputSystemActions.Player.Jump;
+    }
+
+    private void OnEnable()
+    {
+        moveAction.Enable();
+        jumpAction.Enable();
+        jumpAction.performed += OnJump;
+        jumpAction.canceled += OnJumpCanceled;
+    }
+
+    private void OnDisable()
+    {
+        moveAction.Disable();
+        jumpAction.Disable();
+        jumpAction.performed -= OnJump;
+        jumpAction.canceled -= OnJumpCanceled;
+    }
+
+    void Update()
+    {
+        moveInput = moveAction.ReadValue<Vector2>();
+        horizontal = moveInput.x;
+        Flip();
+    }
+
+    private void OnJump(InputAction.CallbackContext context)
+    {
+        if (IsGrounded())
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpingPower);
+        }
+    }
+
+    private void OnJumpCanceled(InputAction.CallbackContext context)
+    {
+        if (rb.linearVelocity.y > 0f)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y * 0.5f);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(horizontal * speed, rb.linearVelocity.y);
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+
+    private void Flip()
+    {
+        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
+    }
+}
